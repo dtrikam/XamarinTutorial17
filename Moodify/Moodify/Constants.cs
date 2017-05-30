3cf4cd298b6bb644154cee2d3a27fdc1202c1855
Dipesh Kumar Trikam <dtri723@aucklanduni.ac.nz>
2017-05-30 13:43:12 +1200

    Can pick an image from photo library

---
+++ Moodify/Moodify/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Moodify
+{
+    public static class Constants
+    {
+        public static readonly string EmotionApiKey = "<INSERT_API_KEY_HERE>";
+    }
+}
--- Moodify/Moodify/Views/FacialRecognition.xaml.cs
+++ Moodify/Moodify/Views/FacialRecognition.xaml.cs
@@ -1,22 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-
+using System.Diagnostics;
+using System.Linq;
 using Xamarin.Forms;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace Moodify
 {
     public partial class FacialRecognition : ContentPage
     {
-        void Handle_Clicked(object sender, System.EventArgs e)
+		//EmotionServiceClient emotionClient;
+		//MediaFile photo;
+
+		public FacialRecognition()
+		{
+			InitializeComponent();
+			Title = "Facial Recognition";
+			//Icon = "faceRec.png";
+			var emotionClient = new EmotionServiceClient(Constants.EmotionApiKey);
+		}
+
+        async void Handle_ClickedAsync(object sender, System.EventArgs e)
         {
-            throw new NotImplementedException();
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("No upload", "Picking a photo is not supported.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() => file.GetStream());
+
         }
 
-        public FacialRecognition()
-        {
-            InitializeComponent();
-            Title = "Facial Recognition";
-            Icon = "faceRec.png";
-        }
+
     }
 }
